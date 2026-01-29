7b443d1094ae8762d98c903eecf80f2c5fe16a81
Pyetro-ZS <pyetromesquita15@gmail.com>
2026-01-29 19:17:32 -0300

    Initialize config commit

---
+++ src/main/java/com/PicPay_Simple/PicPaySimpleApplication.java
@@ -0,0 +1,13 @@
+package com.PicPay_Simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PicPay_Simple/PicPaySimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PicPay_Simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPaySimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
