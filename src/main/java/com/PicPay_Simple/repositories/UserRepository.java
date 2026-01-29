94066842a0e7211064c085af2f0e3e3e148a4776
Pyetro-ZS <pyetromesquita15@gmail.com>
2026-01-29 19:24:06 -0300

    create UserRepository

---
+++ src/main/java/com/PicPay_Simple/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.PicPay_Simple.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.PicPay_Simple.domain.user.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+    
+    Optional<User> findUserById(Long id);
+
+}
