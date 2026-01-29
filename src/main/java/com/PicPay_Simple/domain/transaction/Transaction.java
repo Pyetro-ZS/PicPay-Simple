ee258787f2c2a3d6a05619fa1205c09880917fc2
Pyetro-ZS <pyetromesquita15@gmail.com>
2026-01-29 19:23:15 -0300

    create transaction entity

---
+++ src/main/java/com/PicPay_Simple/domain/transaction/Transaction.java
@@ -0,0 +1,43 @@
+package com.PicPay_Simple.domain.transaction;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.PicPay_Simple.domain.user.User;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
